74c69b36ce5dff8ccda120c3258632c5053c6bf4
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-02 12:25:31 -0400

    gotta pull

--- client/src/components/ConvoCard.js
+++ client/src/components/ConvoCard.js
@@ -1,11 +1,13 @@
-
+import { useNavigate } from 'react-router-dom'
 
 function ConvoCard({convo, user}){
 
-
+    const navigate = useNavigate()
+    
     function handleClick(){
-        console.log('sucka!')
+        console.log(convo.id)
         //navigate to this convo's page
+
     }
     console.log(convo)
 
