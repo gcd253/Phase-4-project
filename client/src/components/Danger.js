9c9ad6fff784b2fbf25fa506f7ae9e33fe450be1
Brendan Ward <kingbeebop@gmail.com>
2022-08-02 12:43:15 -0400

    danger moves

--- client/src/components/Danger.js
+++ client/src/components/Danger.js
@@ -1,10 +1,35 @@
+import { useEffect } from "react";
 
 function Danger(){
 
-    
+    //todo: code timer
+    const display = <h2 class="timer">5:00</h2>
+
+    function startTimer(duration) {
+        let timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+        }   
+        }, 1000)
+    }
+
+    useEffect(()=>{
+        startTimer(300, display)
+    },[])
 
     return <div>
-
+        <h1>A Drone Strike Has Been Initiated On Your Present Location</h1>
+        {display}
+        <img class="background-image" src=""></img>
     </div>
 }
 
