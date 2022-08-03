0a80c54472f69469d9ed0bc2fe9257d753145405
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-03 10:47:53 -0400

    user_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
     validates :message, presence: true
+    validates :user_id, presence: true
+    validates :conversation_id, presence: true
 
     belongs_to :conversation
     belongs_to :user
