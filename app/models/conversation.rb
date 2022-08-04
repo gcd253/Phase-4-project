f5ae24e265f8275d98c32ff83aa757e0eb12ed5b
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-04 17:46:04 -0400

    route for patch

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,6 @@
 class Conversation < ApplicationRecord
+    validates :name, presence: true
+    
     has_many :messages
     has_many :members
     has_many :users, through: :members
--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,6 +1,8 @@
 class ConversationSerializer < ActiveModel::Serializer
   attributes :name, :id, :created_at, :updated_at
 
+
+
   has_many :messages
   has_many :members
   has_many :users, through: :members
