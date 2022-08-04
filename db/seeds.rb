6dcadac5a410e62e7b3dcd705a5a26ff569212aa
Brendan Ward <kingbeebop@gmail.com>
2022-08-04 18:02:06 -0400

    reseeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@ User.create(username: "test", email: "test@test.com", password: "test")
     User.create(username: Faker::Internet.username, email: Faker::Internet.email, password_digest: Faker::Internet.password)
 end
 
-40.times do 
+50.times do 
     Conversation.create(name: Faker::Company.catch_phrase)
 end
 
-100.times do
+1000.times do
     Message.create(message: Faker::Lorem.sentences(number: 1), user_id: User.ids.sample, conversation_id: Conversation.ids.sample)
 end
 
-160.times do
+1000.times do
     Member.create(user: User.all.sample, conversation: Conversation.all.sample)
 end
 
