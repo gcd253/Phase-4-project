a5ce4b1ff3aab75ff8b8a3974bb68f59e8c3f061
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-04 11:29:46 -0400

    dateTime disaster

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -4,6 +4,13 @@ function Message({message, username}){
 
     const [actualMessage, setActualMessage] = useState({user:{username: "loading..."}})
 
+    function formatDateTime(dateString) {
+        const dateTime = new Date(dateString)
+        const array = dateTime.toString().split("", 21)
+
+        return array
+    }
+
     useEffect(()=>{
         fetch(`/messages/${message.id}`)
         .then(res=>res.json())
@@ -17,7 +24,7 @@ function Message({message, username}){
     return <div className={`messages-div${actualMessage.user.username === username ? " user-messages" : ""}`} id={message.id}>
         <p>{actualMessage.user.username} says:</p>
         <p>{message.message}</p>
-        <p>{message.created_at}</p>
+        <p>{formatDateTime(message.created_at)}</p>
     </div>
 }
 
